2ca4e74a535c69b36d6d57e70dc6395752a3bee0
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-05-16 19:45:13 +0300

    Da16

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 import Profileinfo from './Profileinfo/Profileinfo';
+import React from 'react';
 
 const Profile = () => {
   return (
