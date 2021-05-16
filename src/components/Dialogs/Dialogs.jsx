8eeb11589db882d10ca6c2ec48561b41cd92e006
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-05-16 21:22:39 +0300

    das111111

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -43,6 +43,7 @@ const Dialogs = (props) => {
                     <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
                     <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
                     <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                    <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
                 </div>
                 <div className={s.messages}>
                     <Message message={messagesData[0].message}/>
