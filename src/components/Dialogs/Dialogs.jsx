f6ba56124990fb8716f96a4417608e14e8405921
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-05-20 18:11:29 +0300

    Dialogs - myPosts = clic textarea example

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,21 +6,39 @@ import Data from './../../index.js';
 
 const Dialogs = (props) => {
 
-    let DialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
-    let MessageElement = props.state.message.map(m => <Message message={m.message} id={m.id} />);
+  let DialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+  let MessageElement = props.state.message.map(m => <Message message={m.message} id={m.id} />);
 
-    return (
-        <div>
-            <div className={s.dialogs}>
-                <div className={s.dialogsItems}>
-                    {DialogsElements}
-                </div>
-                <div className={s.messages}>
-                    {MessageElement}
-                </div>
-            </div>
+  let newpostE = React.createRef();
+
+  let addP = () => {
+    let texts = newpostE.current.value;
+    alert(texts);
+  }
+
+  return (
+    <div>
+      <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+          {DialogsElements}
         </div>
-    )
+        <div className={s.messages}>
+          {MessageElement}
+        </div>
+        <div>
+          <div>
+            <textarea ref={newpostE}> 
+            </textarea>
+          </div>
+          <div>
+            <button onClick={addP}>
+              clic
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,17 +8,25 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
   let PostsElements = props.posts.map(m => <Post message={m.message} id={m.id} />)
 
-  return (
+  let newPostElement = React.createRef();
+  
+  let addPost = () => {
+    let text = newPostElement.current.value;
+      alert(text);
+    }
+  
+  
+    return (
     <div className={s.postsBlock}>
       <h3>
         My posts
       </h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={s.posts}>
