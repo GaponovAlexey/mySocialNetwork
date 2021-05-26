317253307971a4e3ac63dd5ac30e654609a42235
kekelberi <alexei.gaponov@gmail.com>
2021-05-26 22:39:23 +0300

    37 end

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { addPost, updateNewPostText } from './Redux/State';
 import { BrowserRouter } from "react-router-dom"
 
 
-let rerenderEntireTree = () => {
+let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
                 <App
-                    state={store.getState()}
+                    state={state}
                     addPost={store.addPost.bind(store)}
                     updateNewPostText={store.updateNewPostText.bind(store)}
 
