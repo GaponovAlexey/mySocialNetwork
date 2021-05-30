07004d23494ce6c971dfc33660524f725d5a857d
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-05-30 18:11:10 +0300

    da

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ let rerenderEntireTree = (state) => {
                     state={state}
                     dispatch={store.dispatch.bind(store)}
                     store={store}
-
                 />
             </BrowserRouter>
         </React.StrictMode>,
