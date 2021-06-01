c2c861cc62f20460779efe6e3f0845ed82fbbbf1
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-06-01 20:34:59 +0300

    yes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from "react-router-dom"
-import StoreContext from './StoreContext';
-
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from './StoreContext';
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <StoreContext.Provider value={store}>
+                <Provider store={store}>
                     <App />
-                </StoreContext.Provider>
+                </Provider>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
