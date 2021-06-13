dd9b81aa235020cbdf11ec7a33d69e78e9f90fbb
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-06-13 20:23:53 +0300

    Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,50 +1,25 @@
 import React from 'react';
 import stules from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../asets/images/ilon.jpg';
+
+
 
 let Users = (props) => {
   if (props.users.length === 0) {
 
     axios.get("https://social-network.samuraijs.com/api/1.0/users")
-    .then(response => {
-      debugger;
-      props.setUsers()
-    })
-
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl: 'https://s0.rbk.ru/v6_top_pics/media/img/9/24/756209687115249.jpg',
-        followed: true, fullName: 'Alexey', status: "i'm a boss", location: { city: 'USA', country: 'Ukraine' }
-      },
-      {
-        id: 2,
-        photoUrl: 'https://s0.rbk.ru/v6_top_pics/media/img/9/24/756209687115249.jpg',
-        followed: false, fullName: 'Viktor', status: "i'm a studen", location: { city: 'Russia', country: 'Korea' }
-      },
-      {
-        id: 3,
-        photoUrl: 'https://s0.rbk.ru/v6_top_pics/media/img/9/24/756209687115249.jpg',
-        followed: false, fullName: 'Sergey', status: "i'm a politic", location: { city: 'Ukraine', country: 'USA' }
-      },
-      {
-        id: 4,
-        photoUrl: 'https://s0.rbk.ru/v6_top_pics/media/img/9/24/756209687115249.jpg',
-        followed: true, fullName: 'Masha', status: "i'm a samurai", location: { city: 'UK', country: 'Unete Kindom' }
-      },
-      {
-        id: 5,
-        photoUrl: 'https://s0.rbk.ru/v6_top_pics/media/img/9/24/756209687115249.jpg',
-        followed: false, fullName: 'Inokentiy', status: "i'm a cherry", location: { city: 'Moldowa', country: 'Russia' }
-      },]);
+      .then(response => {
+        props.setUsers(response.data.items)
+      })
   }
-
 
   return <div>
     {
       props.users.map(u => <div key={u.id}>
         <span>
           <div>
-            <img src={u.photoUrl} className={stules.userPhoto} />
+            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={stules.userPhoto} />
           </div>
           <div>
             {u.followed
@@ -54,12 +29,12 @@ let Users = (props) => {
         </span>
         <span>
           <span>
-            <div>{u.fullName}</div>
-            <div>{u.status}</div>
+            <div>{u.name}</div>
+            <div>{u.uniqueUrlName}</div>
           </span>
           <span>
-            <div>{u.location.country}</div>
-            <div>{u.location.city}</div>
+            <div>{u.id}</div>
+            <div>{u.id}</div>
           </span>
         </span>
       </div>)
