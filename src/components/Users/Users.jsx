249e87e82b8d47b8b95acac214f561032ae94c7a
Kekelberi <alexei.gaponov@gmail.com>
2021-06-15 15:46:08 +0300

    componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,45 +3,45 @@ import stules from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../asets/images/ilon.jpg';
 
+class Users extends React.Component {
+		componentDidMount () {
+		axios.get("https://social-network.samuraijs.com/api/1.0/users")
+		.then(response => {
+			this.props.setUsers(response.data.items)
+		})
+	}
 
-
-let Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-          props.setUsers(response.data.items)
-        })
-    }
-  }
-
-  return <div>
-    <button onClick={getUsers}>da</button>
-    {
-      props.users.map(u => <div key={u.id}>
-        <span>
-          <div>
-            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={stules.userPhoto} />
-          </div>
-          <div>
-            {u.followed
-              ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-              : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-          </div>
-        </span>
-        <span>
-          <span>
-            <div>{u.name}</div>
-            <div>{u.uniqueUrlName}</div>
-          </span>
-          <span>
-            <div>{u.id}</div>
-            <div>{u.id}</div>
-          </span>
-        </span>
-      </div>)
-    }
-  </div>
+	render() {
+		return <div>
+			<button onClick={this.getUsersssss}>da</button>
+			{
+				this.props.users.map(u => <div key={u.id}>
+					<span>
+						<div>
+							<img src={u.photos.small != null ? u.photos.small : userPhoto} className={stules.userPhoto} />
+						</div>
+						<div>
+							{u.followed
+								? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+								: <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+						</div>
+					</span>
+					<span>
+						<span>
+							<div>{u.name}</div>
+							<div>{u.uniqueUrlName}</div>
+						</span>
+						<span>
+							<div>{u.id}</div>
+							<div>{u.id}</div>
+						</span>
+					</span>
+				</div>)
+			}
+		</div>
+	}
 }
+
+
 
 export default Users;
