e9a9fb0aff68035ac8057e84a6e32bd4b933427e
Kekelberi <alexei.gaponov@gmail.com>
2021-06-16 15:58:00 +0300

    постраничный вывод

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -1,14 +1,18 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 
 let initialState = {
-  users: [
-  ],
+  users: [],
+  pageSize: 5,
+  totalUsersCount: 21,
+  currentPage: 1,
+
 };
 
 
-const usersReducer = (state = initialState, action) => {
+const  usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case FOLLOW:
       return {
@@ -34,8 +38,9 @@ const usersReducer = (state = initialState, action) => {
       return {
         ...state,
         users: [ ...action.users],
-        //users: [...state.users, ...action.users],
       }
+    case SET_CURRENT_PAGE:
+      return {...state, currentPage: action.currentPage}
     default:
       return state;
   }
@@ -44,5 +49,6 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
 export const setUsersAC = (users) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage });
 
 export default usersReducer;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,48 +4,56 @@ import * as axios from 'axios';
 import userPhoto from '../../asets/images/ilon.jpg';
 
 class Users extends React.Component {
-	
-		componentDidMount () {
-		debugger;
-			axios.get("https://social-network.samuraijs.com/api/1.0/users")
-		.then(response => {
-			this.props.setUsers(response.data.items)
-		})
+
+	componentDidMount() {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+			.then(response => {
+				this.props.setUsers(response.data.items)
+			})
 	}
 	render() {
+		let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+		let pages = [];
+		for (let i=1; i <= pagesCount; i++) {
+			pages.push(i);
+		}
+	
 		return <div>
-			<div>
-				<span>1</span>
-				<span className={stules.SeletedPage}>2</span>
-				<span>3</span>
-				<span>4</span>
-				<span>5</span>
-			</div>
-			{
-				this.props.users.map(u => <div key={u.id}>
-					<span>
-						<div>
-							<img src={u.photos.small != null ? u.photos.small : userPhoto} className={stules.userPhoto} />
-						</div>
-						<div>
-							{u.followed
-								? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
-								: <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-						</div>
-					</span>
-					<span>
-						<span>
-							<div>{u.name}</div>f
-							<div>{u.uniqueUrlName}</div>
-						</span>
-						<span>
-							<div>{u.id}</div>
-							<div>{u.id}</div>
-						</span>
-					</span>
-				</div>)
-			}
+		<div>
+			{ pages.map(p => {
+				return <span 
+					className={this.props.currentPage === p && stules.SeletedPage}
+				onClick={() => {this.props.setCurrentPage(p)}}
+				>{p}</span>
+
+			})}
 		</div>
+		{
+			this.props.users.map(u => <div key={u.id}>
+				<span>
+					<div>
+						<img src={u.photos.small != null ? u.photos.small : userPhoto} className={stules.userPhoto} />
+					</div>
+					<div>
+						{u.followed
+							? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+							: <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+					</div>
+				</span>
+				<span>
+					<span>
+						<div>{u.name}</div>f
+						<div>{u.uniqueUrlName}</div>
+					</span>
+					<span>
+						<div>{u.id}</div>
+						<div>{u.id}</div>
+					</span>
+				</span>
+			</div>)
+		}
+		</div >
 	}
 }
 
