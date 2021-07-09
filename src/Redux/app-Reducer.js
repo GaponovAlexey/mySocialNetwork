49d1d59000c2965d15bffbb88d3bf2dc5af4b5c4
Kekelberi <alexei.gaponov@gmail.com>
2021-07-09 15:52:20 +0300

    dawd

--- src/Redux/app-Reducer.js
+++ src/Redux/app-Reducer.js
@@ -1,4 +1,4 @@
-//import { getAuthUserData } from "./auth-Reducer";
+import { getAuthUserData } from "./auth-reducer";
 
 const INITIALIZED_SUCCESS = 'INITIALIZED_SUCCESS';
 
@@ -24,7 +24,8 @@ const appReducer = (state = initialState, action) => {
 export const initializedSuccess = () => ({ type: INITIALIZED_SUCCESS });
 
 export const initializeApp = () => (dispatch) => {
-	//let dispatchResult = dispatch(getAuthUserData());
+	let dispatchResult = dispatch(getAuthUserData());
+	debugger;
 
 	dispatch(initializedSuccess())
 
