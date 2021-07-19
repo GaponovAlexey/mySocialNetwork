b279d91c46023ead17a1d4f014596ea6c1b4df12
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-07-19 14:53:59 +0300

    musiccomponts

--- src/components/Test/test.js
+++ src/components/Test/test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './test.module.css';
 import { NavLink } from 'react-router-dom';
+import poKaifu from '../../asets/music/mp3/poKaifu.mp3'
 
 const test = (props) => {
 
@@ -39,33 +40,50 @@ const test = (props) => {
 
 
 	return (
-		<div className={ s.tHeadClass }>
-			<div>
+		<>
+			<div className={ s.tHeadClass }>
 				<div>
+					<div>
+					</div>
+					<table>
+						<textarea
+							value={ newMessageBodyTest }
+							onChange={ updateNewMessageText }
+							placeholder='you man' />
+						<tr>
+							<button onClick={ sendMessge }>clic</button>
+						</tr>
+					</table>
 				</div>
-				<textarea
-					value={ newMessageBodyTest }
-					onChange={ updateNewMessageText }
-					placeholder='you man'
-				/>
+				<div>
+					{ Man }
+					{ Woman }
+				</div>
+				<div className={ s.one }>
+				</div>
+				<div>
+					<iframe width="700"
+						height="400" src='http://localhost:3000/one' srcdoc />
+				</div>
 			</div>
 			<div>
-				<button
-					onClick={ sendMessge }
-				>
-					clic
-				</button>
 			</div>
-			<div className={ s.one }>
-				{ Man }
-			</div>
-			<div>
-				{ Woman }
-			</div>
-			<div>
-				{ Woman }
-			</div>
-		</div>
+			<body>
+				<fieldset>
+					<legend>Пол</legend>
+					Муж. <input type="radio" name="sex" value="1" />
+					Жен. <input type="radio" name="sex" value="2" />
+					Иное. <input type="radio" name="sex" value="3" />
+					анимешник. <input type="radio" name="sex" value="3" />
+					транс. <input type="radio" name="sex" value="3" />
+				</fieldset>
+				<audio src={ poKaifu } controls preload="metadata">
+					<p>Ваш Web-браузер не поддерживает элемент audio</p>
+				</audio>
+
+			</body>
+
+		</>
 	)
 }
 export default test;
